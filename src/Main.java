33dd14b3bbeca459b0028d7be1e6b37d6906d773
gevago01 <gevagorou@gmail.com>
2017-10-12 14:51:38 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.TreeMap;
 
 public class Main {
 
+    //where the loop terminates
+    private int series_to;
+    //holds the density at each level
+    private Map<Integer, Double> densities = new TreeMap<>();
     //number of entries in a node
     private final static int f = 40;
     //dimensionality
@@ -42,32 +46,46 @@ public class Main {
 //    private final static double q_i = 2 * Math.pow(10, -3);//w0
 //    private final static double q_i = 6 * Math.pow(10, -3);//w1
     private final static double q_i = 2 * Math.pow(10, -2);//w2
-    private static Map<String,Double> dataset_root_densities = new TreeMap<>();
+    private static Map<String, Double> dataset_root_densities = new TreeMap<>();
+
     static {
-        dataset_root_densities.put("abs02",0.69951);
-        dataset_root_densities.put("bit02",0.0);
-        dataset_root_densities.put("dia02",0.000002);
-        dataset_root_densities.put("par02",0.449276);
-        dataset_root_densities.put("ped02",0.0);
-        dataset_root_densities.put("pha02",0.0);
-        dataset_root_densities.put("rea02",0.1216);
-        dataset_root_densities.put("uni02",0.0);
+        dataset_root_densities.put("abs02", 0.69951);
+        dataset_root_densities.put("bit02", 0.0);
+        dataset_root_densities.put("dia02", 0.000002);
+        dataset_root_densities.put("par02", 0.449276);
+        dataset_root_densities.put("ped02", 0.0);
+        dataset_root_densities.put("pha02", 0.0);
+        dataset_root_densities.put("rea02", 0.1216);
+        dataset_root_densities.put("uni02", 0.0);
     }
 
-    private int series_to;
+    private final static double ROOT_DENSITY = dataset_root_densities.get("uni02");
 
-    private Map<Integer, Double> densities = new TreeMap<>();
+    /**
+     * Calculates logarithm of a number with any base
+     *
+     * @param base
+     * @param num
+     * @return
+     */
+    public double logOfBase(int base, double num) {
+        return Math.log(num) / Math.log(base);
+    }
 
-    private final static double ROOT_DENSITY = dataset_root_densities.get("uni02") ;
-
-    public int logOfBase(int base, double num) {
-        double to = Math.ceil(Math.log(num) / Math.log(base));
+    /**
+     * Returns the ceiling
+     *
+     * @param base
+     * @param num
+     * @return
+     */
+    public int myceil(int base, double num) {
+        double to = Math.ceil(logOfBase(base, num));
         return (int) to;
-
     }
 
     Main() {
-        series_to = 1 + logOfBase(f, N / f);
+        series_to = 1 + myceil(f, N / f);
         calculateDensitiesAtEachLevel();
         System.out.println("f:" + f);
         System.out.println("n:" + n);
@@ -79,23 +97,28 @@ public class Main {
      * Calculates the density at level 0 and then at each level.
      */
     public void calculateDensitiesAtEachLevel() {
-
+        //ROOT_DENSITY is a constant that
+        // has to be defined in the class body
         densities.put(0, ROOT_DENSITY);
         for (int i = 1; i < series_to; i++) {
             double density_at_i = 1 + ((Math.pow(densities.get(i - 1), 1.0 / n) - 1) / Math.pow(f, 1.0 / n));
-            densities.put(i,density_at_i);
+            densities.put(i, density_at_i);
         }
 
     }
 
-
+    /**
+     * Calculates theodoridi's model
+     *
+     * @return disk_accesses
+     */
     public double calculateModel() {
         double first_term = N / f, sum = 0;
 
         for (int j = 0; j < series_to; j++) {
             double p = 1;
             for (int i = 0; i < n; i++) {
-                double product_term = Math.pow((densities.get(j)*f/N), 1.0/n);
+                double product_term = Math.pow((densities.get(j) * f / N), 1.0 / n);
                 p *= (product_term + q_i);
             }
             sum += (first_term * p);
@@ -108,6 +131,6 @@ public class Main {
         Main main = new Main();
 
         double ret = main.calculateModel();
-        System.out.printf("%.5f", ret);
+        System.out.printf("Disk Accesses:%.5f", ret);
     }
 }
