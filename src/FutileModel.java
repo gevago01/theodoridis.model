2fcf5b4fa0095fdc21a4ffbb6e0617de53e666ec
gevago01 <gevagorou@gmail.com>
2018-01-22 13:27:30 +0000

    commit

--- src/FutileModel.java
+++ src/FutileModel.java
@@ -17,6 +17,7 @@ public class FutileModel {
         objectsDensities.put("pha02", 0.0);
         objectsDensities.put("rea02", 0.1216);
         objectsDensities.put("uni02", 0.0);
+        objectsDensities.put("my02", 0.0101);
 
         objectsDensities.put("abs03", 0.69);
         objectsDensities.put("bit03", 0.0);
@@ -31,23 +32,24 @@ public class FutileModel {
 
 
     /********************change these to get the probability******************/
-    private final static String DATASET="abs03";
+    private final static String DATASET="uni02";
     private final static double objectDensity = objectsDensities.get(DATASET);
     private final static int FANOUT = 70;
-    private final static int DIMENSIONALITY = 3;
+    private final static int DIMENSIONALITY = 2;
     private final static double leafNodeDensity = Math.pow((1+((Math.pow(objectDensity,1.0/DIMENSIONALITY)-1)/(Math.pow(FANOUT,1.0/DIMENSIONALITY)))),DIMENSIONALITY);
-    private final static double DATASET_SIZE = 11958999;
-    private static double queryArea = 1e-6;
-    private static double spaceArea = 100;
-    private static double g = 3000;
+    private final static double DATASET_SIZE = 1e6;
+//    10000 for my
+    private static double queryArea = 1e-3;
+    private static double spaceArea = 1;
+    private static double g = 1000;
     /************************************************************************/
-
-    private static long L;
 
     private static double queryExtent_i = Math.pow(queryArea, 1.0 / DIMENSIONALITY);
     private static double leafNodeArea= leafNodeDensity * FANOUT  / DATASET_SIZE;
+//    private static double leafNodeArea= 3.5e-5;
     //    private static double leafNodeArea = 54.2 * spaceArea / Math.pow(g, DIMENSIONALITY);
     private static double leafNodeExtent_i = Math.pow(leafNodeArea, 1.0 / DIMENSIONALITY);
+    private static long L = (int) Math.pow(Math.ceil(1 + queryExtent_i / leafNodeExtent_i), DIMENSIONALITY);
 
 
 
@@ -55,7 +57,7 @@ public class FutileModel {
         int numberOfLeafNodes = (int)Math.ceil(DATASET_SIZE/FANOUT);
 
         double ln_extent_i = Math.pow(leafNodeArea,1.0/DIMENSIONALITY);
-        return numberOfLeafNodes * Math.pow((ln_extent_i+queryExtent_i),DIMENSIONALITY)/spaceArea;
+        return numberOfLeafNodes * Math.pow((ln_extent_i+queryExtent_i),DIMENSIONALITY)/ spaceArea;
     }
 
 
@@ -63,7 +65,7 @@ public class FutileModel {
     FutileModel() {
         System.out.println("LeafNodeArea:");
         System.out.println(leafNodeArea);
-        L = (int) Math.pow(Math.ceil(1 + queryExtent_i / leafNodeExtent_i), DIMENSIONALITY);
+
 //        L = (int) Math.pow(Math.ceil(1 + Math.sqrt(retrieveEmptyArea()) / leafNodeExtent_i), DIMENSIONALITY);
         System.out.println("L is:" + L);
     }
@@ -74,6 +76,8 @@ public class FutileModel {
         for (long i = 1; i <= L; i++) {
 //            System.out.println(calculateProbability(i));
             double pro = (1.0 / L) * calculateProbability(i);
+//
+//            double pro =  retrieveEmptyArea()/leafNodeArea;
 //            System.out.println(pro);
 //            if (!(Double.isNaN(pro) || pro>1 || pro<0)) {
             sum += pro;
@@ -87,10 +91,22 @@ public class FutileModel {
         return (1.0 - objectDensity)  * Math.pow((Math.pow(spaceArea, 1.0 / DIMENSIONALITY)/g + queryExtent_i), DIMENSIONALITY);
     }
 
+    public void printValues(){
+
+        System.out.println("Leaf node density:"+leafNodeDensity);
+        if (retrieveEmptyArea() > leafNodeArea){
+            System.out.println("Query empty area is bigger than leaf node area");
+        }
+        else{
+            System.out.println("Leaf node area is bigger than query empty area");
+        }
+        System.out.println("this:"+(retrieveEmptyArea()/L) / leafNodeArea);
+    }
+
     private double calculateProbability(long numOfLeafNodes) {
 //        System.out.println(FANOUT + " AND "+numOfLeafNodes+" gives "+((double)FANOUT / numOfLeafNodes));
 //        System.out.println(queryArea+ " AND "+numOfLeafNodes+" gives "+(queryArea / numOfLeafNodes));
-        double d = Math.pow((1 - ((retrieveEmptyArea() / numOfLeafNodes) / (leafNodeDensity * FANOUT / DATASET_SIZE))), ((double) FANOUT / numOfLeafNodes));
+        double d = Math.pow((1 - ((retrieveEmptyArea() / numOfLeafNodes) / (leafNodeArea))), ((double) FANOUT / numOfLeafNodes));
         if (Double.isNaN(d)) {
             System.out.println("-------------------");
             System.out.println(queryArea);
@@ -100,15 +116,21 @@ public class FutileModel {
             System.out.println(FANOUT);
             System.out.println(DATASET_SIZE);
             System.out.println("-------------------");
+            System.exit(9);
         }
-        return Math.pow((1 - ((retrieveEmptyArea() / numOfLeafNodes) / (leafNodeDensity * FANOUT / DATASET_SIZE))), ((double) FANOUT / numOfLeafNodes));
+        return Math.pow((1 - ((retrieveEmptyArea() / numOfLeafNodes) / (leafNodeArea))), ((double) FANOUT / numOfLeafNodes));
     }
 
     public static void main(String[] args) {
         FutileModel mm = new FutileModel();
         double ds_prob = mm.calculateModel();
+        double leafNodeAccesses = mm.predictLeafNodeAccesses();
+        mm.printValues();
+        System.out.println("Theodoridis:"+mm.predictLeafNodeAccesses());
         System.out.println(ds_prob);
-        System.out.println(mm.predictLeafNodeAccesses());
+//        System.out.println("Dead space probability:"+ds_prob);
+//        System.out.println("# of leaf node accesses (Theodoridis):"+leafNodeAccesses);
+//        System.out.println("Futile accesses per query:"+(leafNodeAccesses*ds_prob));
 
 
     }
